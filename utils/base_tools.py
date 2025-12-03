d0d885f21bfe163838aaaf6505e5e69f2203d382
walcheng <40264048+walcheng@users.noreply.github.com>
2025-12-03 20:56:03 +0800

    Add loss functions and early stopping utility
    Implement various loss functions and utilities for training models.

---
+++ utils/base_tools.py
@@ -0,0 +1,183 @@
+import torch
+import torch.nn.functional as F
+import torch.nn as nn
+import numpy as np
+
+
+combinations = [[0, 1], [0, 2], [1, 2]]
+
+
+
+def dis_loss(sep_emb_i, sep_emb_j, sep_emb_k):
+
+    overall_loss = []
+    hid_embed_list = [sep_emb_i, sep_emb_j, sep_emb_k]
+    for i in range(len(hid_embed_list)):
+        loss = HSIC(hid_embed_list[combinations[i][0]], hid_embed_list[combinations[i][1]])
+        overall_loss.append(loss)
+
+    return overall_loss
+
+
+def sim_loss(hid_embed_list):
+    # 排列组合mixup的元素
+    overall_loss = []
+    # L2 norm
+    for i, hid_emb in enumerate(hid_embed_list):
+        hid_embed_list[i] = F.normalize(hid_embed_list[i], p=2.0, dim=1)
+    for i in range(len(hid_embed_list)):
+        embed_cat = hid_embed_list[combinations[i][0]] - hid_embed_list[combinations[i][1]]
+        loss = torch.mean(torch.norm(embed_cat, p='fro', dim=1))
+        overall_loss.append(loss)
+
+    return overall_loss
+
+
+def pairwise_distances(x):
+    # x should be two dimensional
+    instances_norm = torch.sum(x ** 2, -1).reshape((-1, 1))
+    return -2 * torch.mm(x, x.t()) + instances_norm + instances_norm.t()
+
+
+def GaussianKernelMatrix(x, sigma=1):
+    pairwise_distances_ = pairwise_distances(x)
+    return torch.exp(-pairwise_distances_ / sigma)
+
+
+def HSIC(x, y, s_x=1, s_y=1):
+    m, _ = x.shape  # batch size
+    K = GaussianKernelMatrix(x, s_x)
+    L = GaussianKernelMatrix(y, s_y)
+    H = torch.eye(m) - 1.0 / m * torch.ones((m, m))
+    H = H.cuda()
+    HSIC = torch.trace(torch.mm(L, torch.mm(H, torch.mm(K, H)))) / ((m - 1) ** 2)
+    return HSIC
+
+
+class AngularPenaltySMLoss(nn.Module):
+
+    def __init__(self, in_features, out_features, loss_type='arcface', eps=1e-7, s=None, m=None):
+        '''
+        Angular Penalty Softmax Loss
+
+        Three 'loss_types' available: ['arcface', 'sphereface', 'cosface']
+        These losses are described in the following papers: 
+        
+        ArcFace: https://arxiv.org/abs/1801.07698
+        SphereFace: https://arxiv.org/abs/1704.08063
+        CosFace/Ad Margin: https://arxiv.org/abs/1801.05599
+
+        '''
+        super(AngularPenaltySMLoss, self).__init__()
+        loss_type = loss_type.lower()
+        assert loss_type in  ['arcface', 'sphereface', 'cosface']
+        if loss_type == 'arcface':
+            self.s = 64.0 if not s else s
+            self.m = 0.5 if not m else m
+        if loss_type == 'sphereface':
+            self.s = 64.0 if not s else s
+            self.m = 1.35 if not m else m
+        if loss_type == 'cosface':
+            self.s = 30.0 if not s else s
+            self.m = 0.4 if not m else m
+        self.loss_type = loss_type
+        self.in_features = in_features
+        self.out_features = out_features
+        self.fc = nn.Linear(in_features, out_features, bias=False)
+        self.eps = eps
+
+    def forward(self, x, labels):
+        '''
+        input shape (N, in_features)
+        '''
+        assert len(x) == len(labels)
+        assert torch.min(labels) >= 0
+        assert torch.max(labels) < self.out_features
+        
+        for W in self.fc.parameters():
+            W = F.normalize(W, p=2, dim=1)
+
+        x = F.normalize(x, p=2, dim=1)
+
+        wf = self.fc(x)
+        if self.loss_type == 'cosface':
+            numerator = self.s * (torch.diagonal(wf.transpose(0, 1)[labels]) - self.m)
+        if self.loss_type == 'arcface':
+            numerator = self.s * torch.cos(torch.acos(torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1.+self.eps, 1-self.eps)) + self.m)
+        if self.loss_type == 'sphereface':
+            numerator = self.s * torch.cos(self.m * torch.acos(torch.clamp(torch.diagonal(wf.transpose(0, 1)[labels]), -1.+self.eps, 1-self.eps)))
+
+        excl = torch.cat([torch.cat((wf[i, :y], wf[i, y+1:])).unsqueeze(0) for i, y in enumerate(labels)], dim=0)
+        denominator = torch.exp(numerator) + torch.sum(torch.exp(self.s * excl), dim=1)
+        L = numerator - torch.log(denominator)
+        return -torch.mean(L)
+
+
+def log_writer(log_file, s):
+    f = open(log_file, 'a')
+    f.write(s + '\n')
+    f.close()
+
+
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+
+    def __init__(self, patience=7, verbose=False, delta=0, path='checkpoint.pt', trace_func=print):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement.
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+            path (str): Path for the checkpoint to be saved to.
+                            Default: 'checkpoint.pt'
+            trace_func (function): trace print function.
+                            Default: print
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+        self.path = path
+        self.trace_func = trace_func
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            self.trace_func(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        """Saves model when validation loss decrease."""
+        if self.verbose:
+            self.trace_func(
+                f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        if torch.cuda.device_count() > 1:
+            torch.save(model.module.state_dict(), self.path)
+        else:
+            torch.save(model.state_dict(), self.path)
+        self.val_loss_min = val_loss
+
+if __name__ == '__main__':
+    a = AngularPenaltySMLoss(4,3, loss_type='arcface')
+    b = torch.rand((5,4))
+    label = torch.tensor([0,1,2,1,2])
+    a(b, label)
+
+
